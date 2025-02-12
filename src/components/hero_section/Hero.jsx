c5e0b5ed1598c31ce6387548b657b4e129788a28
Karmpatel18 <karmpatel1203@gmail.com>
2025-02-12 15:36:06 +0530

    hero title sm c

--- src/components/hero_section/Hero.jsx
+++ src/components/hero_section/Hero.jsx
@@ -4,8 +4,8 @@ function Hero() {
     return (
         <div className="flex flex-col mt-24 ">
 
-            <div className='absolute inset-x-0 text-4xl mt-6 leading-9 text-center tracking-tighter  font-bold bg-gradient-to-r from-neutral-900 via-neutral-600 to-neutral-400 bg-clip-text text-transparent '>
-                Your one-stop solution <div className="italic font-normal tracking-tighter text-xl"> to search, borrow, and manage books seamlessly!</div>
+            <div className='flex justify-center absolute inset-x-0  mt-6   '>
+                <div className="flex flex-col leading-9 text-center text-4xl tracking-tighter  font-bold bg-gradient-to-r from-neutral-900 via-neutral-600 to-neutral-400 bg-clip-text text-transparent">Your one-stop solution <div className="italic font-normal tracking-tighter text-xl"> to search, borrow, and manage books seamlessly!</div></div>
 
             </div>
             <div className='relative mt-4 z-[-1] flex scale-[68%] sm:scale-[100%] justify-center'>
